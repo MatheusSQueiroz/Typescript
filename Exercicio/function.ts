fe32f5bcab0b112d2eaed240d013a857ef18623e
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-20 12:18:55 -0300

    Utilizando funções

---
+++ Exercicio/function.ts
@@ -0,0 +1,20 @@
+// Nome completo 
+function nomecompleto(nome: string, sobrenome: string){
+    console.log(nome + ' ' + sobrenome)
+}
+nomecompleto('Matheus', 'Queiroz')
+
+
+// Multiplicando 
+function multiplicacao(num1: number, num2: number){
+console.log(num1 * num2)
+}
+
+multiplicacao(9,3)
+
+// Nome e idade 
+function nomeidade(nome: string, idade: number){
+    console.log(nome + ' \nidade: ' + idade)
+}
+
+nomeidade('Matheus', 23)
